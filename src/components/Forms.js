58f7f7973828a70ef8542c1d4adcfa8c0c2c1aaa
harshdev2909 <harsh.dev2909@gmail.com>
2024-08-30 21:41:11 +0530

    Updated

--- src/components/Forms.js
+++ src/components/Forms.js
@@ -2,8 +2,28 @@ import React, { useState } from 'react'
 
 export const Forms = () => {
 const[name,setName] =useState('');
+const [email,setEmail] = useState('');
+const [password,setpassword] = useState('');
+const [confirmPassword,setConfirmPassword] = useState('');
+const [age,setAge] = useState('');
+const[errors,setErrors]=useState('')
+
+const validateForm =()=>{
+    const error = {};
+    if(!name){
+        error.name='name is required'
+    }
+    if(!email){
+        error.name='Email is required'
+    }
+    console.log(error)
+}
     const handleSubmit =(e)=>{
         e.preventDefault();
+        if(Object.keys(validate).length>0){
+            setErrors(validate);
+        }
+        const validate = validateForm();
         console.log('form submited')
     }
   return (
@@ -12,23 +32,24 @@ const[name,setName] =useState('');
        <form onSubmit={handleSubmit}>
         <div>
             <label>Name</label>
-            <input onChange={(e)=>setName(e.target.value)}  type='text'/>
+            <input onChange={(e)=>setName(e.target.value)} type='text'/>
+            {Error.name}
         </div>
         <div>
             <label>Email</label>
-            <input type='email'/>
+            <input type='email'onChange={(e)=>setEmail(e.target.value)}/>
         </div>
         <div>
             <label>Password</label>
-            <input type='password'/>
+            <input type='password'onChange={(e)=>setpassword(e.target.value)}/>
         </div>
         <div>
             <label>Confirm Password</label>
-            <input type='password'/>
+            <input type='password'onChange={(e)=>setConfirmPassword(e.target.value)}/>
         </div>
         <div>
             <label>Age</label>
-            <input type='number'/>
+            <input type='number'onChange={(e)=>setAge(e.target.value)}/>
         </div>
         <div>
             <label>Gender</label>
